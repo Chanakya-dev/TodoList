80bdc3b16a1280b180239f82b436bd70ac6cb272
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-12-17 16:12:39 +0530

    todo app updated with simple code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoApp from './todoList';
+
 import Calendar from './calendar/components/Calendar';
+import TodoAppNew from './todoApp/TodoApp';
+
 
 function App() {
   return (
     <div className="App">
-      <TodoApp/>
+    <TodoAppNew/>
     </div>
   );
 }
